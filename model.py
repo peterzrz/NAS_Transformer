21d0b497ac67eafc5cb491023a7df0d94d927511
beloiual <beloiual@umich.edu>
2023-11-29 16:53:08 -0500

    Added the transformer and modifed the model

--- model.py
+++ model.py
@@ -1,18 +1,28 @@
 from keras.models import Model
 from keras.layers import Input, Dense, Conv2D, GlobalAveragePooling2D
+from transformer import Transformer
+
+
 
 # generic model design
 def model_fn(actions):
     # unpack the actions from the list
-    kernel_1, filters_1, kernel_2, filters_2, kernel_3, filters_3, kernel_4, filters_4 = actions
+    kernel_1, filters_1, act_1, kernel_2, filters_2, kernel_3, filters_3, kernel_4, filters_4 = actions
 
-    ip = Input(shape=(32, 32, 3))
-    x = Conv2D(filters_1, (kernel_1, kernel_1), strides=(2, 2), padding='same', activation='relu')(ip)
-    x = Conv2D(filters_2, (kernel_2, kernel_2), strides=(1, 1), padding='same', activation='relu')(x)
-    x = Conv2D(filters_3, (kernel_3, kernel_3), strides=(2, 2), padding='same', activation='relu')(x)
-    x = Conv2D(filters_4, (kernel_4, kernel_4), strides=(1, 1), padding='same', activation='relu')(x)
-    x = GlobalAveragePooling2D()(x)
-    x = Dense(10, activation='softmax')(x)
+    num_layers = 4
+    d_model = 128
+    dff = 512
+    num_heads = 8
+    dropout_rate = 0.1
 
-    model = Model(ip, x)
+    model = Transformer(
+        num_layers=num_layers,
+        d_model=d_model,
+        num_heads=num_heads,
+        dff=dff,
+        input_vocab_size=7765,
+        target_vocab_size=7010,
+        dropout_rate=dropout_rate,
+        activation_encoder=[],
+        activation_decoder=[])
     return model
